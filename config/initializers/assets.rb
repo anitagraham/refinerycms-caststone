d7cda6c9fabf774eec361032c842ecc5a900f0a9
Anita Graham <anita@joli.com.au>
2015-05-06 19:31:05 +0800

    Use assets.rb to define assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,4 @@ Refinery::Core.configure do |config|
   # Register extra stylesheet for backend (optional options)
   config.register_stylesheet "refinerycms-caststone.css", :media => 'screen'
 end
+Rails.application.config.assets.precompile += %w( refinerycms-caststone.css refinerycms-caststone.js )
