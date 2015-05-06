1f67d74ba7b6205d8bcb044f6e1ecc1ee9f0386e
Anita Graham <anita@joli.com.au>
2015-05-06 21:16:02 +0800

    fake commit - last one got lost

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,6 @@ Refinery::Core.configure do |config|
   config.register_javascript "refinerycms-caststone.js"
 
   # Register extra stylesheet for backend (optional options)
-  config.register_stylesheet "refinerycms-caststone.css", :media => 'screen'
+  config.register_stylesheet "refinerycms-caststone.css",:media => 'screen'
 end
 Rails.application.config.assets.precompile += %w( refinerycms-caststone.css refinerycms-caststone.js )
