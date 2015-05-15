c08d82011cd33a8bd742e18cad3934cb4b84760e
Anita Graham <anita@joli.com.au>
2015-05-15 11:36:06 +0800

    Recommended method of redefining page_params

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -2,13 +2,20 @@ Refinery::Admin::PagesController.class_eval do
 
 	before_filter :find_all_caststone_objects , :only => [:create, :edit]
 
-  def page_params_with_photo_params
-    photo_params = params.require(:page).permit(photo_ids:[])
-    page_params_without_photo_params.merge(photo_params)
-  end
+  # work around an issue with stack level too deep, due to an issue with decorators.
+  if self.instance_methods.exclude?(:page_params_with_photo_params)
+    # We need to add photos attributes to page_params as it is ignored by strong parameters. (See #100)
+    def page_params_with_photo_params
 
-  # Swap out the default page_params method with our new one
-  alias_method_chain :page_params, :photo_params
+      # Get the :images_attributes hash from params
+      photo_params = params.require(:page).permit(:photos_show, :photos_count, :photos_select)
+      page_params_without_photo_params.merge(photo_params)
+
+    end
+
+    # Swap out the default page_params method with our new one
+    alias_method_chain :page_params, :photo_params
+  end
 
 
 	protected
