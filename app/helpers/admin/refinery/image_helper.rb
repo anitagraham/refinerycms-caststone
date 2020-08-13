0e9eabdae4f2b83a8bed16883fd95e2ab9f915df
Anita Graham <anita@joli.com.au>
2020-08-13 22:29:04 +0800

    Change tracking_id to come from database

--- app/helpers/admin/refinery/image_helper.rb
+++ app/helpers/admin/refinery/image_helper.rb
@@ -9,13 +9,14 @@ module Refinery
       thumbnail_args[:geometry] = geometry if geometry
       image_tag_args = (image.thumbnail_dimensions(geometry) rescue {})
       image_tag_args[:alt] = image.respond_to?(:title) ? image.title : image.image_name
-      image_tag(image.thumbnail(thumbnail_args).url, image_tag_args.merge(options)) << tracking_id(image)
+
+      image_tag(image.thumbnail(thumbnail_args).url, image_tag_args.merge(options)) << tag.span(image.tracking_id.presence, class: 'tracking_id')
 
     end
 
-    def tracking_id(image)
-      tid = [image.title, image.alt, image.image_name.split('.').first].find{ |name| name.match(TRACKING_ID)}
-      tid.blank? ? tid : tag.span(tid, class: 'tracking_id')
-    end
+    # def tracking_id(image)
+    #   tid = [image.title, image.alt, image.image_name.split('.').first].find{ |name| name.match(TRACKING_ID)}
+    #   tid.blank? ? tid : tag.span(tid, class: 'tracking_id')
+    # end
   end
 end
