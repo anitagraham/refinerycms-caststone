b5a7baf0938a580322eaf2d2f602dac7f708791a
Anita Graham <anita@joli.com.au>
2020-08-30 19:01:25 +0800

    Rename tracking_id to photo_number everywhere

--- db/migrate/17_add_photo_number_to_photo.rb
+++ db/migrate/17_add_photo_number_to_photo.rb
@@ -1,4 +1,4 @@
-class AddTrackidToPhoto < ActiveRecord::Migration[6.0]
+class AddPhotoNumberToPhoto < ActiveRecord::Migration[6.0]
   def change
     add_column :refinery_caststone_photos, :photo_number, :string
   end
