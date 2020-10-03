2a1ba6d9fe73113f3e93ede31773e84882203834
Anita Graham <anita@joli.com.au>
2020-10-03 16:17:14 +0800

    add WymEditor style for various buttons

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -4,8 +4,7 @@
 // editor's boot process for that key, e.g. skin: 'something_else'
   var custom_visual_editor_boot_options = {
     classesItems: [
-      {name: 'button-download', rules:['button', 'download'], join: '-'},
-      {name: 'button-cta',      rules:['button', 'cta'], join: '-'},
+      {name: 'button', rules:['download', 'cta', 'phone'], join: '-'},
       {name: 'text-align',      rules:['left', 'center', 'right', 'justify'], join: '-'},
       {name: 'image-align',     rules:['left', 'right'], join: '-'},
       {name: 'font-size',       rules:['small','normal','large'], join: '-'},
